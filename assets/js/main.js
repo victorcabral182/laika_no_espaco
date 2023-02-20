e52032c1dcaedc5ff5a8edcaf67f0761062b0ac1
victorcabral182 <107046713+victorcabral182@users.noreply.github.com>
2023-02-20 12:18:01 -0300

    Added responsiveness for ipad/tablets.

--- assets/js/main.js
+++ assets/js/main.js
@@ -5,7 +5,9 @@ buttonMenu.addEventListener("click", () => {
     containerMenu.classList.toggle("container__menu-links-ativo");
     if(containerMenu.classList.length > 1) {
         buttonMenu.innerHTML = "&#128473;"
+        buttonMenu.style.marginBottom = "3px"
     } else {
         buttonMenu.innerHTML = "&#9776;"
+        buttonMenu.style.marginBottom = "-3px"
     }
 })
